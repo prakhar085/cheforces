a2857bb0687434bbee193e9ee5aff56eb04023f2
“Cricsudheer” <cricsudheer@gmai.com>
2021-06-24 17:46:08 +0530

    firstcommit

---
+++ cheforces/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+
+class codeforcesform(forms.Form):
+    CF_handle= forms.CharField()
---
+++ cheforces/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect, HttpResponse
+from . import forms
+import urllib.request as request
+import json
+
+
+# getting api response
+def get_api_response(url):
+    try:
+        with request.urlopen(url) as response:
+
+            source = response.read()
+            data = json.loads(source)
+            data = data['result']
+            return data
+
+    except:
+        print("wow")
+
+
+def homepage(request):
+    return render(request, 'cheforces/base.html')
+
+
+# codeforces open page
+def cf_openpage(request):
+    form = forms.codeforcesform(request.POST or None)
+    if request.method == "POST":
+        form = forms.codeforcesform(request.POST)
+        if form.is_valid():
+            handle = form.cleaned_data.get('CF_handle')
+            return redirect('/cfuser/' + handle)
+        else:
+            form = forms.codeforcesform()
+
+    return render(request, 'cheforces/home.html', {'form': form})
+
+
+# codeforces homepage
+
+
+def cf_home(request, handle):
+    # getting basic user info
+    url = "https://codeforces.com/api/user.info?handles=" + handle
+
+    # getting user submission info
+    userinfo = get_api_response(url)
+    url = "https://codeforces.com/api/user.status?handle=" + handle
+    user_submissions = get_api_response(url)
+
+    # extracting data for verdicts and ratings pie chart
+
+    verdicts = {}
+    ratings = {}
+    for i in range(len(user_submissions)):
+        verdicts[user_submissions[i]['verdict']] = verdicts.get(user_submissions[i]['verdict'], 0) + 1
+        ratings[user_submissions[i]['problem']['rating']] = ratings.get(user_submissions[i]['problem']['rating'], 0) + 1
+
+
+
+
+    verdicts_labels = list(verdicts.keys())
+    verdicts_data = list(verdicts.values())
+
+    ratings_labels = list(ratings.keys())
+    ratings_data = list(ratings.values())
+    return render(request, 'cheforces/cfhome.html', {'userinfo': userinfo[0],
+                                                     "verdicts_labels": verdicts_labels,
+                                                     "verdicts_data": verdicts_data,
+                                                     "ratings_label":ratings_labels,
+                                                     "ratings_data":ratings_data})
