4f9924bd9878dbf9f28a3bd44c281b0d18bfd976
Prafull Sahu <psahu81011@gmail.com>
2021-06-26 02:15:06 +0530

    added unsolved problems

--- cheforces/views.py
+++ cheforces/views.py
@@ -4,6 +4,7 @@ import urllib.request as request
 import json
 import datetime
 
+
 # getting api response
 def get_api_response(url):
     try:
@@ -19,29 +20,26 @@ def get_api_response(url):
 
 
 def homepage(request):
-
-    return render(request, 'cheforces/base.html',)
+    return render(request, 'cheforces/base.html', )
 
 
 # codeforces open page
 def cf_openpage(request):
-    #upcoming contests
+    # upcoming contests
     url = "https://codeforces.com/api/contest.list?gym=false"
     upcoming_contest = get_api_response(url)
-    upcoming_contest=upcoming_contest[0:20]
-    before=[]
-    completed=[]
+    upcoming_contest = upcoming_contest[0:20]
+    before = []
+    completed = []
     for i in range(len(upcoming_contest)):
-        upcoming_contest[i]['startTimeSeconds']=datetime.datetime.fromtimestamp(int(upcoming_contest[i]['startTimeSeconds'])).strftime('%Y-%m-%d %H:%M:%S')
-        if upcoming_contest[i]['phase']=="BEFORE":
+        upcoming_contest[i]['startTimeSeconds'] = datetime.datetime.fromtimestamp(
+            int(upcoming_contest[i]['startTimeSeconds'])).strftime('%Y-%m-%d %H:%M:%S')
+        if upcoming_contest[i]['phase'] == "BEFORE":
             before.append(upcoming_contest[i])
-        else :
+        else:
             completed.append(upcoming_contest[i])
 
-
-
-
-    #search form
+    # search form
     form = forms.codeforcesform(request.POST or None)
     if request.method == "POST":
         form = forms.codeforcesform(request.POST)
@@ -52,8 +50,8 @@ def cf_openpage(request):
             form = forms.codeforcesform()
 
     return render(request, 'cheforces/home.html', {'form': form,
-                                                   "before":before,
-                                                   "completed" : completed[0:min(len(completed),7)]})
+                                                   "before": before,
+                                                   "completed": completed[0:min(len(completed), 7)]})
 
 
 # codeforces homepage
@@ -72,50 +70,61 @@ def cf_home(request, handle):
 
     verdicts = {}
     ratings = {}
-    question_tags={}
+    question_tags = {}
     langs = {}
+    w_ans = {}
+    c_ans = {}
     for i in range(len(user_submissions)):
         if 'verdict' in user_submissions[i]:
+            key = '{}/problem/{}/'.format(user_submissions[i]['problem']['contestId'], user_submissions[i]['problem']['index'])
+            if user_submissions[i]['verdict'] == "OK":
+                if key in w_ans:
+                    del(w_ans[key])
+                c_ans[key] = 1
+            else:
+                w_ans[key] = 1
+                if key in c_ans:
+                    del(w_ans[key])
             verdicts[user_submissions[i]['verdict']] = verdicts.get(user_submissions[i]['verdict'], 0) + 1
-            if "rating" in user_submissions[i]["problem"]  and  user_submissions[i]['verdict']=="OK":
-                ratings[user_submissions[i]['problem']['rating']] = ratings.get(user_submissions[i]['problem']['rating'], 0) + 1
+            if "rating" in user_submissions[i]["problem"] and user_submissions[i]['verdict'] == "OK":
+                ratings[user_submissions[i]['problem']['rating']] = ratings.get(
+                    user_submissions[i]['problem']['rating'], 0) + 1
                 for j in range(len(user_submissions[i]['problem']['tags'])):
-                    question_tags[user_submissions[i]['problem']['tags'][j]] = question_tags.get(user_submissions[i]['problem']['tags'][j],0)+1
-            if user_submissions[i]['verdict']=="OK":
-                if user_submissions[i]['programmingLanguage'] in langs :                
-                    langs[user_submissions[i]['programmingLanguage']]+=1 
-                else :
-                    langs[user_submissions[i]['programmingLanguage']]=1
+                    question_tags[user_submissions[i]['problem']['tags'][j]] = question_tags.get(
+                        user_submissions[i]['problem']['tags'][j], 0) + 1
+            if user_submissions[i]['verdict'] == "OK":
+                if user_submissions[i]['programmingLanguage'] in langs:
+                    langs[user_submissions[i]['programmingLanguage']] += 1
+                else:
+                    langs[user_submissions[i]['programmingLanguage']] = 1
 
+    # tags and verdicts labels for drawing pie chart
 
-
-
-    #tags and verdicts labels for drawing pie chart
-    
-    ratings_data  = [['RATING','COUNT']]
+    ratings_data = [['RATING', 'COUNT']]
     ratings_data.extend(dict_to_list(ratings))
 
-    tags_data = [['TAGS','DATA']]
+    tags_data = [['TAGS', 'DATA']]
     tags_data.extend(dict_to_list(question_tags))
 
     verdicts_data = [['VERDICTS', 'COUNT']]
     verdicts_data.extend(dict_to_list(verdicts))
 
-    langs_data = [['LANGUAGE','COUNT']]
+    langs_data = [['LANGUAGE', 'COUNT']]
     langs_data.extend(dict_to_list(langs))
 
     return render(request, 'cheforces/cfhome.html', {'userinfo': userinfo[0],
                                                      "verdicts_data": verdicts_data,
-                                                     "ratings_data":ratings_data,
+                                                     "ratings_data": ratings_data,
                                                      "tags_data": tags_data,
-                                                     "langs_data": langs_data
+                                                     "langs_data": langs_data,
+                                                     "w_ans": w_ans,
+                                                     "solved": len(c_ans),
                                                      })
 
 
-
 def dict_to_list(dict):
-    l=[]
-    for i,j in dict.items():
-        l.append([str(i),j])
-    l = sorted(l, key=lambda x: x[1],reverse=True)
+    l = []
+    for i, j in dict.items():
+        l.append([str(i), j])
+    l = sorted(l, key=lambda x: x[1], reverse=True)
     return l
